6547b41ab831bc8cbb88e5fda608659a02bb184d
nitriques <nico@nitriques.com>
2015-10-26 23:57:18 -0400

    sql queries + table ui

--- extension.driver.php
+++ extension.driver.php
@@ -20,6 +20,12 @@
 		 * @var string
 		 */
 		const EXT_NAME = 'Restricted Sections';
+
+		/**
+		 * Name of the table
+		 * @var string
+		 */
+		const TBL_NAME = 'tbl_authors_restricted_sections';
 		
 		/**
 		 * Symphony utility function that permits to
@@ -118,12 +124,39 @@
 
 		public function addCustomAuthorColumn(array $context)
 		{
-
+			$context['columns'][] = array(
+				'label' => __('Allowed Sections'),
+				'sortable' => false,
+				'handle' => 'allowed-sections'
+			);
 		}
 
 		public function addCustomAuthorColumnData(array $context)
 		{
-
+			$author = $context['author'];
+			$text = __('None');
+			$inactive = true;
+			if ($author->isDeveloper() ||
+				$author->isPrimaryAccount()) {
+				$text = '*';
+			}
+			else {
+				$count = static::fetchCount($author->get('id'));
+				$inactive = $count === 0;
+				if (!$inactive) {
+					if ($count === 1) {
+						$text = __('Only 1 section');
+					}
+					else {
+						$text = __('%d sections', array($count));
+					}
+				}
+			}
+			$class = null;
+			if ($inactive) {
+				$class = 'inactive';
+			}
+			$context['tableData'][] = Widget::TableData($text, $class);
 		}
 
 
@@ -134,12 +167,12 @@
 			// Takes privileges to edit this
 			if (!$curAuthor->isDeveloper() &&
 				!$curAuthor->isManager() &&
-				!$curAuthor->isPrimary()) {
+				!$curAuthor->isPrimaryAccount()) {
 				return;
 			}
 			// Even manager should not edit their own
 			if (!$curAuthor->isDeveloper() &&
-				!$curAuthor->isPrimary() &&
+				!$curAuthor->isPrimaryAccount() &&
 				$curAuthor->get('id') == $author->get('id')) {
 				return;
 			}
@@ -148,7 +181,6 @@
 			'author' => $author,
 			'fields' => $_POST['fields'],
 			*/
-			//var_dump($context['form']);die;//getChild
 
 			$sections = SectionManager::fetch();
 
@@ -160,34 +192,34 @@
 
 		public function authorPreDelete(array $context)
 		{
-
+			// Nothing to do
 		}
 
 		public function authorPostDelete(array $context)
 		{
-
+			// TODO: delete all infos about this author
 		}
 
 
 		public function authorPreCreate(array $context)
 		{
-
+			static::validate(static::getSectionsFromPOST(), $context['errors']);
 		}
 
 		public function authorPostCreate(array $context)
 		{
-
+			static::save(static::getSectionsFromPOST(), $context['author']);
 		}
 
 
 		public function authorPreEdit(array $context)
 		{
-
+			static::validate(static::getSectionsFromPOST(), $context['errors']);
 		}
 
 		public function authorPostEdit(array $context)
 		{
-
+			static::save(static::getSectionsFromPOST(), $context['author']);
 		}
 
 
@@ -200,7 +232,7 @@
 			*/
 			$curAuthor = Symphony::Author();
 			if ($curAuthor->isDeveloper() ||
-				$curAuthor->isPrimary()) {
+				$curAuthor->isPrimaryAccount()) {
 				return;
 			}
 			$page = Administration::instance()->Page;
@@ -235,7 +267,7 @@
 			$attributes = array(
 				'multiple' => 'multiple',
 				'class' => 'required',
-				'required' => 'required',
+				//'required' => 'required',
 			);
 			$options = array(
 				array(0, false, __('All Sections'))
@@ -244,9 +276,15 @@
 			foreach ($sections as $section) {
 				$options[] = array($section->get('id'), false, $section->get('name'));
 			}
-			$select = Widget::Select('restr_section[sections]', $options, $attributes);
+			$select = Widget::Select('restr_section[sections][]', $options, $attributes);
 			$label->appendChild($select);
-			$group->appendChild($label);
+
+			if ($_SERVER['REQUEST_METHOD'] === 'POST' && !static::isValid()) {
+				$group->appendChild(Widget::Error($label, 'Error'));
+			}
+			else {
+				$group->appendChild($label);
+			}
 
 			return $group;
 		}
@@ -262,15 +300,74 @@
 			return isset($count[0]) && (int)$count[0] > 0;
 		}
 
-		protected static function save()
+		protected static function getSectionsFromPOST() {
+			if (!isset($_POST['restr_section']) ||
+				!isset($_POST['restr_section']['sections']) ||
+				!is_array($_POST['restr_section']['sections'])) {
+				return null;
+			}
+			return $_POST['restr_section']['sections'];
+		}
+
+		protected static function isValid($section_ids)
 		{
-			
+			return $section_ids !== null && is_array($section_ids) && !empty($section_ids);
+		}
+
+		protected static function validate($section_ids, &$errors)
+		{
+			if (!static::isValid($section_ids)) {
+				if (is_array($errors)) {
+					$error['restr_section'] = 'Please select a value for Restricted Section';
+				}
+				return false;
+			}
+			return true;
+		}
+
+		protected static function save(array $section_ids, Author $author)
+		{
+			$ret = Symphony::Database()->delete(self::TBL_NAME, '1=0');
+			foreach ($section_ids as $section_id) {
+				if ($section_id === null || $section_id == '' || !ctype_digit($section_id)) {
+					continue;
+				}
+				$ret = Symphony::Database()->insert(array(
+					'author_id' => intval($author->get('id')),
+					'section_id' => intval($section_id),
+				), self::TBL_NAME, true);
+			}
+			return true;
+		}
+
+		protected static function fetch($author_id)
+		{
+			return Symphony::Database()->fetch(sprintf("
+				SELECT `id`, `section_id` FROM `%s`
+					WHERE `author_id` = %d
+			", self::TBL_NAME, intval($author_id)));
+		}
+
+		protected static function fetchCount($author_id)
+		{
+			$count = Symphony::Database()->fetchCol('c', sprintf("
+				SELECT count(`id`) as c FROM `%s`
+					WHERE `author_id` = %d
+			", self::TBL_NAME, intval($author_id)));
+			if (is_array($count)) {
+				$count = current($count);
+			}
+			if (!$count) {
+				return 0;
+			}
+			return intval($count);
 		}
 
 		protected static function createTable()
 		{
+			$tbl = self::TBL_NAME;
 			$ret = Symphony::Database()->query("
-				CREATE TABLE `tbl_authors_restricted_sections` (
+				CREATE TABLE `$tbl` (
 					`id` int(11) unsigned NOT NULL auto_increment,
 					`author_id` int(11) unsigned NOT NULL,
 					`section_id` int(11) unsigned NOT NULL,
@@ -283,7 +380,8 @@
 
 		protected static function dropTable()
 		{
-			$ret = Symphony::Database()->query("DROP TABLE `tbl_authors_restricted_sections`");
+			$tbl = self::TBL_NAME;
+			$ret = Symphony::Database()->query("DROP TABLE `$tbl`");
 			return $ret;
 		}
 
